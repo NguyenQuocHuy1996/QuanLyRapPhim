3ca5e815ebc519034846c76f85f75da440c3e15c
NguyenQuocHuy1996 <shermeilove@gmail.com>
2017-05-11 00:21:08 +0700

    Upload DTO

---
+++ DTO/KhachHangDTO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DTO
+{
+    public class KhachHangDTO
+    {
+        public string TaiKhoan { get; set; }
+        public string MatKhau { get; set; }
+        public string HoTen { get; set; }
+        public string GioiTinh { get; set; }
+        public string NgaySinh { get; set; }
+        public int CMND { get; set; }
+        public string DiaChi { get; set; }
+        public string Email { get; set; }
+        public int SDT { get; set; }
+
+        public KhachHangDTO(string taiKhoan, string matKhau, string hoTen, string gioiTinh, string ngaySinh, int cmnd, string diaChi, string email, int sdt)
+        {
+            TaiKhoan = taiKhoan;
+            MatKhau = matKhau;
+            HoTen = hoTen;
+            GioiTinh = gioiTinh;
+            NgaySinh = ngaySinh;
+            CMND = cmnd;
+            DiaChi = diaChi;
+            Email = email;
+            SDT = sdt;
+        }
+
+        public KhachHangDTO()
+        {
+
+        }
+    }
+}
---
+++ DTO/RapPhimDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DTO
+{
+    public class RapPhimDTO
+    {
+        public string MaRapPhim { get; set; }
+        public string TenRapPhim { get; set; }
+        public string DiaChi { get; set; }
+        public string MaNV { get; set; }
+        public string TenNhanVien { get; set; }
+
+        public RapPhimDTO(string maRapPhim, string tenRapPhim, string diaChi, string maNV, string tenNV)
+        {
+            MaRapPhim = maRapPhim;
+            TenRapPhim = tenRapPhim;
+            DiaChi = diaChi;
+            MaNV = maNV;
+            TenNhanVien = tenNV;
+        }
+
+        public RapPhimDTO()
+        {
+
+        }
+    }
+}
